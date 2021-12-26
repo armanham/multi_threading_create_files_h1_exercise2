5893dfd3ec0ff22582b0e6930aa16d99322037a4
Arman2232 <82290609+Arman2232@users.noreply.github.com>
2021-12-26 21:25:50 +0400

    Update FileUtils.java

--- src/com/company/FileUtils.java
+++ src/com/company/FileUtils.java
@@ -2,7 +2,7 @@ package com.company;
 
 public class FileUtils {
 
-    public static final String BASE_PATH = "C:\\Users\\Admin\\Desktop\\IntellIjProjects\\multi threading_create files_exercise2\\src\\files";
+    public static final String BASE_PATH = "D:\\IntellIjProjects\\multi threading_create files_h1_exercise2\\src\\files\\";
 
     private FileUtils() {
         throw new UnsupportedOperationException();
